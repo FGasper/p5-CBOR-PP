9fac173103f44e4b90b0746c1ecfa80620fc4660
Felipe Gasper <felipe@felipegasper.com>
2019-05-12 14:47:03 -0400

    0.03 release

--- lib/CBOR/PP.pm
+++ lib/CBOR/PP.pm
@@ -76,7 +76,7 @@ This code is licensed under the same license as Perl itself.
 
 #----------------------------------------------------------------------
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use CBOR::PP::Encode ();
 use CBOR::PP::Decode ();
